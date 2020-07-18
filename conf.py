866966a9d4dd83cf5efe57fbc4fdac1dd8d6a969
avinashgedela <57897359+avinashgedela@users.noreply.github.com>
2020-07-18 14:37:29 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Web Design and Development - Apssdc'
-copyright = '2020, APSSDC'
-author = 'APSSDC'
+project = 'Avinash'
+copyright = '2020, Avinash'
+author = 'Avinash'
 
 # The full version, including alpha/beta/rc tags
 release = '1'
